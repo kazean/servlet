8308c891df79fb619baf0ac96b99dea0e5e0a0d8
kazean <yeogunna@gmail.com>
2022-01-26 18:35:33 +0900

    spring mvc

---
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberListControllerV1.java
@@ -0,0 +1,26 @@
+package hello.servlet.web.springmvc.v1;
+
+import hello.servlet.domain.Member;
+import hello.servlet.domain.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@Controller
+public class SpringMemberListControllerV1 {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members")
+    public ModelAndView process(HttpServletRequest request, HttpServletResponse response){
+        List<Member> members = memberRepository.findAll();
+        ModelAndView mav = new ModelAndView("members");
+        mav.addObject("members", members);
+        return mav;
+    }
+}
